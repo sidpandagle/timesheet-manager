bd09c45a0b9a60702e211762f01fff01be7fb1ff
Siddhant Pandagle <siddhant.pandagle1998@gmail.com>
2024-11-14 19:36:55 +0000

    Basic Dashboard Layout

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,167 +1,27 @@
 import { Link } from "@remix-run/react";
 
 export default function Dashboard() {
-  return (<div>
-    <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-      <div className="px-3 py-3 lg:px-5 lg:pl-3">
-        <div className="flex items-center justify-between">
-          <div className="flex items-center justify-start rtl:justify-end">
-            <button
-              data-drawer-target="logo-sidebar"
-              data-drawer-toggle="logo-sidebar"
-              aria-controls="logo-sidebar"
-              type="button"
-              className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            >
-              <span className="sr-only">Open sidebar</span>
-            </button>
-            <a href="https://flowbite.com" className="flex ms-2 md:me-24">
-              <img
-                src="https://flowbite.com/docs/images/logo.svg"
-                className="h-8 me-3"
-                alt="FlowBite Logo"
-              />
-              <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
-                Flowbite
-              </span>
-            </a>
+  const arr = [...new Array(10)];
+  return (
+    <div className="flex bg-slate-900 text-slate-500 min-h-screen">
+      <div className="border-r border-slate-500 flex flex-col gap-1">
+      <div className="h-16 flex items-center px-4">Logo</div>
+        {arr.map(r=>{
+          return (<button className="p-2 m-2 hover:bg-slate-400 rounded-md duration-100">Logo</button>)
+        })}
+      </div>
+      <div className="flex-1">
+        <div className="h-16 border-b  border-slate-500 flex items-center px-4">
+          <div className="text-3xl">Dashboard</div>
+        </div>
+        <div className="flex h-[calc(100%-4rem)]">
+          <div className="w-3/4 border-r  border-slate-500 p-4">
+            <div>Timer</div>
+            <div>Projects</div>
           </div>
-          <div className="flex items-center">
-            <div className="flex items-center ms-3">
-              <div>
-                <button
-                  type="button"
-                  className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-                  aria-expanded="false"
-                  data-dropdown-toggle="dropdown-user"
-                >
-                  <span className="sr-only">Open user menu</span>
-                  <img className="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user" />
-                </button>
-              </div>
-              <div
-                className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600"
-                id="dropdown-user"
-              >
-                <div className="px-4 py-3" role="none">
-                  <p
-                    className="text-sm text-gray-900 dark:text-white"
-                    role="none"
-                  >
-                    Neil Sims
-                  </p>
-                  <p
-                    className="text-sm font-medium text-gray-900 truncate dark:text-gray-300"
-                    role="none"
-                  >
-                    neil.sims@flowbite.com
-                  </p>
-                </div>
-                <ul className="py-1" role="none">
-
-                  <Link
-                    to="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                    role="menuitem"
-                  >
-                    Dashboard
-                  </Link>
-
-                  <Link
-                    to="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                    role="menuitem"
-                  >
-                    Settings
-                  </Link>
-
-                  <Link
-                    to="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                    role="menuitem"
-                  >
-                    Earnings
-                  </Link>
-
-                  <Link
-                    to="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                    role="menuitem"
-                  >
-                    Sign out
-                  </Link>
-
-                </ul>
-              </div>
-            </div>
-          </div>
+          <div className="p-4">Activity</div>
         </div>
       </div>
-    </nav>
-    <aside
-      id="logo-sidebar"
-      className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
-      aria-label="Sidebar"
-    >
-      <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
-        <ul className="space-y-2 font-medium">
-
-          <Link
-            to="#"
-            className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-          >
-            <span className="ms-3">Dashboard</span>
-          </Link>
-
-          <Link
-            to="#"
-            className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-          >
-            <span className="flex-1 ms-3 whitespace-nowrap">Kanban</span>
-          </Link>
-          <span className="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300">
-            Pro
-          </span>
-
-          <Link
-            to="#"
-            className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-          >
-            <span className="flex-1 ms-3 whitespace-nowrap">Inbox</span>
-            <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
-              3
-            </span>
-          </Link>
-          <Link to="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <span className="flex-1 ms-3 whitespace-nowrap">Users</span>
-          </Link>
-          <Link
-            to="#"
-            className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-          ><span className="flex-1 ms-3 whitespace-nowrap">Products</span>
-          </Link>
-          <Link
-            to="#"
-            className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-          >
-            <span className="flex-1 ms-3 whitespace-nowrap">Sign In</span>
-          </Link>
-
-          <Link
-            to="#"
-            className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-          >
-            <span className="flex-1 ms-3 whitespace-nowrap">Sign Up</span>
-          </Link>
-        </ul>
-      </div>
-    </aside>
-    <div className="p-4 sm:ml-64">
-      <div className="p-4 mt-12">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam delectus minus, in corrupti cumque voluptas, aspernatur optio blanditiis ab neque nostrum accusantium nulla, atque dolorum quaerat autem fugit placeat ipsum!
-      </div>
     </div>
-  </div>
-
   )
 }
