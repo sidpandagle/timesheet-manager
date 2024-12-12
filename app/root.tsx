f93541464a5da5dd031bf3a2515cf50d8773bb15
fudge-fantastic <siddhant.pandagle1998@gmail.com>
2024-12-13 00:04:39 +0530

    Shadcn calendar and combobox added

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import type { LinksFunction } from "@remix-run/node";
 
 import "./tailwind.css";
 import { ThemeProvider } from "./themetoggle";
+import styles from "./tailwind.css?url"
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -21,6 +22,7 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  { rel: "stylesheet", href: styles },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
